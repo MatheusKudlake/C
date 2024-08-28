1ac5fdc62624c8454e01a2f66d99613130d32a13
MatheusKudlake <mkudlake@gmail.com>
2024-08-28 19:18:34 -0300

    Add files via upload

--- Exercicios-sorteaveis/5.c
+++ Exercicios-sorteaveis/5.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int num, dezena;
+    int num, res;
  
     printf("Digite um numero de 3 digitos:\n");
     scanf("%d", &num);
-    
-    while (num > 9){
-        dezena = num % 10;
-        num /= 10;
+
+    if (num < 100 || num > 999){ 
+        printf("O numero deve possuir 3 casas decimais.\n");
+        return 1;
     }
 
-    printf("A casa da dezena desse numero eh: %d", dezena);
+    res = (num/10) % 10;
 
+    printf("A casa das dezenas do numero eh %d.\n", res);
     return 0;
 }
