7e66771ed548d89570b22dba4ba4e3cb63e5eb74
MatheusKudlake <mkudlake@gmail.com>
2024-11-06 21:33:41 -0300

    Add files via upload

--- lista04/5.c
+++ lista04/5.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int lin, col;
+    int lin, col, nulos=0;
     printf("Insira a quantidade de linhas da matriz:\n");
     scanf("%d", &lin);
     printf("Insira a quantidade de colunas da matriz:\n");
@@ -10,12 +10,22 @@ int main()
 
     int matriz[lin][col];
 
-    printf("Insira os ")
+    printf("Insira os numeros da matriz:\n");
 
     for(int l=0; l<lin; l++){
         for(int c=0; c<col; c++){
-            scanf("")
+            scanf("%d", &matriz[l][c]);
+            if(matriz[l][c]==0){
+                nulos++;
+            }
         }
     }
+    for(int l=0; l<lin; l++){
+        for(int c=0; c<col; c++){
+            printf("[%d]", matriz[l][c]);
+        }
+        printf("\n");
+    }
+    printf("Numero de valores nulos: %d", nulos);
     return 0;
 }
